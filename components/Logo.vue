b6bd5d0500077aa67310dd78718dbd8049d67541
Ryan Binns <hi@rbi.nz>
2019-11-30 18:00:36 +0800

    refactor(logo.vue): remove unused animations
    remove unused animations and update legacy class name

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="VueToBinzLogo">
+  <div class="rbinzLogo">
     <div class="Triangle Triangle--two" />
     <div class="Triangle Triangle--two--lighten" />
     <div class="Triangle Triangle--one" />
@@ -11,7 +11,7 @@
 </template>
 
 <style>
-.VueToBinzLogo {
+.rbinzLogo {
   display: inline-block;
   position: relative;
   overflow: hidden;
@@ -28,21 +28,18 @@
 }
 
 .Triangle--one {
-  animation: transitionOne 0.5s linear forwards 3.5s;
   border-left: 105px solid transparent;
   border-right: 105px solid transparent;
   border-top: 180px solid #83e0a4;
 }
 
 .Triangle--one--lighten {
-  animation: transitionOneHighlight 0.5s linear forwards 3.5s;
   border-left: 105px solid transparent;
   border-top: 180px solid #98eab5;
 }
 
 .Triangle--two {
   left: 70px;
-  animation: gorightThree 0.5s linear forwards 3.5s;
   border-left: 87.5px solid transparent;
   border-right: 87.5px solid transparent;
   border-top: 150px solid #43a1c5;
@@ -50,14 +47,12 @@
 
 .Triangle--two--lighten {
   left: 70px;
-  animation: gorightThreeHighlight 0.5s linear forwards 3.5s;
   border-left: 87.5px solid transparent;
   border-top: 150px solid #7dc6e1;
 }
 
 .Triangle--three {
   left: 70px;
-  animation: gorightTwo 0.5s linear forwards 3.5s;
   border-left: 70px solid transparent;
   border-right: 70px solid transparent;
   border-top: 120px solid #184757;

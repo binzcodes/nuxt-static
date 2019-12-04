a3264a8aa120cb3244a730aea671f6943ab05c36
Ryan Binns <hi@rbi.nz>
2019-12-04 11:56:56 +0800

    refactor(components): refactor components to new structure
    organise components as described in new documented standard

---
+++ components/placeholder/Stack.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card-actions v-if="stack.length > 0" class="pb-8">
+    <div v-bind:key="stack.index" v-for="technology in stack" class="mx-auto">
+      <app-technology v-bind:technology="technology"></app-technology>
+    </div>
+  </v-card-actions>
+</template>
+
+<script>
+export default {
+  name: 'Stack',
+  components: {
+    AppTechnology: () => import('~/components/placeholder/Technology.vue')
+  },
+  props: {
+    stack: {
+      type: [Object, Array],
+      default: () => []
+    }
+  }
+}
+</script>
--- components/placeholder/Technology.vue
+++ components/placeholder/Technology.vue
@@ -1,19 +1,19 @@
 <template>
   <v-tooltip bottom>
     <template v-slot:activator="{ on }">
-      <v-icon v-on="on" :color="skill.color" size="28">
-        {{ skill.icon }}
+      <v-icon v-on="on" :color="technology.color" size="28">
+        {{ technology.icon }}
       </v-icon>
     </template>
-    <span>{{ skill.name }}</span>
+    <span>{{ technology.name }}</span>
   </v-tooltip>
 </template>
 
 <script>
 export default {
-  name: 'Skill',
+  name: 'Technology',
   props: {
-    skill: {
+    technology: {
       type: Object,
       required: true
     }
